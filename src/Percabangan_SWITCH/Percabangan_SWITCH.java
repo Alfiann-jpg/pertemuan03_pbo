1cdf473e7a6a7848257edc7b9f12f17fc5b9407c
alfiann <08874326455.com>
2021-04-12 04:28:16 +0800

    Percabangan_SWITCH

---
+++ src/Percabangan_SWITCH/Percabangan_SWITCH.java
@@ -0,0 +1,25 @@
+package Percabangan_SWITCH;
+
+
+public class Percabangan_SWITCH {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("uang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            default:
+                System.out.println("mahluk halus");
+                break;
+        }
+    }
+}
