382b230aa76c22a5bb405ba95b70161fbf1bf4d4
alfiann <08874326455.com>
2021-04-11 23:49:08 +0800

    buat class operasistring

---
+++ src/operasistring.java
@@ -0,0 +1,11 @@
+package string;
+
+
+public class operasistring {
+    public static void main(String[] args) {
+        string kota = "yogyakarta";
+    }
+
+        System.out.println(kota);
+    }
+}
