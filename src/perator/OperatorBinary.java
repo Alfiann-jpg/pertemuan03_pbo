0d40cb09e2d6608bfc4e01eb89cb0001654580ce
alfiann <08874326455.com>
2021-04-12 03:52:51 +0800

    Operator Binary Aritmatika

--- src/perator/OperatorBinary.java
+++ src/perator/OperatorBinary.java
@@ -3,8 +3,18 @@ package perator;
 
 public class OperatorBinary {
 
-   string nama      = "Muhammad Alfiannor";
+   String nama      = "Muhammad Alfiannor";
    int    jumlahsks = 24;
    double IPK       = 3.89;
+
+   public static void main(String[] args){
+
+      System.out.println(9+3);
+      System.out.println(9-3);
+      System.out.println(9*3);
+      System.out.println(9/3);
+      System.out.println(9%3);
+      System.out.println(9%2);
+   }
 
 }
